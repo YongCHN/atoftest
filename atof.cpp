6af258d5f915913c9b6ec1239a0d3c9b609c21af
YongCHN <YongCHN@users.noreply.github.com>
2014-05-13 09:26:00 +0800

    remove blank spaces

--- atof.cpp
+++ atof.cpp
@@ -13,22 +13,50 @@ int toInt(char c)
 	return c - '0';
 }
 
+void trim(const char * p, int & headPos, int & tailPos)
+{
+	int len = strlen(p);
+
+	bool headFound = false;
+	bool tailFound = false;
+
+	headPos = 0;
+	tailPos = 0;
+
+	for (int i = 0; (i < len) && !(headFound && tailFound); i++)
+	{
+		if (!headFound && p[i] == ' ')
+			headPos++;
+		else
+			headFound = true;
+
+		if (!tailFound && p[len - i - 1] == ' ')
+			tailPos++;
+		else
+			tailFound = true;
+	}
+}
+
 double atof(const char * p)
 {
 	if (p == NULL) throw bad_exception("p as null");
 
-	int len = strlen(p);
+	int head = 0;
+	int bottom = 0;
+	trim(p, head, bottom);
+	int len = strlen(p) - head - bottom;
 	if (len == 0) throw bad_exception("p is zero length");
 
-	bool is_negative = p[0] == '-';
+	len += head;
+	bool is_negative = p[head] == '-';
 	bool is_dot = false;
 	double franction = 0;
 	int integer = 0;
 	double percentage = 1.0;
 	 
-	len = p[len - 1] == 'f' ? len - 1 : len;
+	len = ((p[len - 1] == 'f') || (p[len - 1] == 'F')) ? len - 1 : len;
 
-	for (int i = is_negative ? 1 : 0; i < len; i++)
+	for (int i = is_negative ? head + 1 : head; i < len; i++)
 	{
 		if (p[i] == '.')
 		{
@@ -73,6 +101,7 @@ void validate(double expected, const char * input)
 int _tmain(int argc, _TCHAR* argv[])
 {
 	validate(1.2, "1.2");
+	validate(1.2, " 1.2 ");
 	validate(1.2, "1.2f");
 	validate(-1.2, "-1.2");
 	validate(-.2, "-.2");
